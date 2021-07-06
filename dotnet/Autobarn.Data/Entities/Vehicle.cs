ecf9c3ca222e230258b3118f753434ddbc1b000a
Dylan Beattie <dylan@dylanbeattie.net>
2021-07-06 14:11:28 +0100

    Initialize pub-sub

---
+++ dotnet/Autobarn.Data/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+
+namespace Autobarn.Data.Entities {
+	public partial class Vehicle {
+		public string Registration { get; set; }
+		public string ModelCode { get; set; }
+		public string Color { get; set; }
+		public int Year { get; set; }
+
+		public virtual Model VehicleModel { get; set; }
+	}
+}
